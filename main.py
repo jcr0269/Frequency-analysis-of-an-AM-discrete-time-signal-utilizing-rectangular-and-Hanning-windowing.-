70a32a92afdb9d2514ac6c267596d8a14fe6e9a4
Jared Riley <2JRemail@gmail.com>
2022-10-24 21:33:11 -0500

    finished the project for hw 4

--- main.py
+++ main.py
@@ -9,10 +9,23 @@ f2 = 5/128
 fc = 50/128
 sr = 128
 
+#0<n<255
 t = np.linspace(0,255,255)
 xn = np.cos(2*pi*(1/18)*t) + np.cos(2*pi*f2*t)
 xc = np.cos(2*pi*fc*t)
 xam = xn*xc
+
+#0<n<99
+t1 = np.linspace(0,99,99)
+xn1 = np.cos(2*pi*(1/18)*t1) + np.cos(2*pi*f2*t1)
+xc1 = np.cos(2*pi*fc*t1)
+xam1 = xn1*xc1
+
+#0<n<179
+t2 = np.linspace(0,179,179)
+xn2 = np.cos(2*pi*(1/18)*t2) + np.cos(2*pi*f2*t2)
+xc2 = np.cos(2*pi*fc*t2)
+xam2 = xn2*xc2
 
 def DFT(x):
     """
@@ -20,7 +33,6 @@ def DFT(x):
     discrete Fourier Transform
     of a 1D real-valued signal x
     """
-
     N = len(x)
     n = np.arange(N)
     k = n.reshape((N, 1))
@@ -29,16 +41,30 @@ def DFT(x):
     X = np.dot(e, x)
     return X
 
-def hanning(x):
-    han = np.hanning(len(xam))
-    A = fft(han)
+def DFT1(x, point):
+    A=fft(x, point)
     mag = np.abs(fftshift(A))
     freq1 = np.linspace(-0.5, 0.5, len(A))
     response = 20 * np.log10(mag)
     response = np.clip(response, -100, 100)
     return freq1, response
 
-freq1, response = hanning(xam)
+
+def hanning(x, point):
+    han = np.hanning(len(xam))
+    han = (han*xam)
+    A = fft(han, point)
+    mag = np.abs(fftshift(A))
+    freq1 = np.linspace(-0.5, 0.5, len(A))
+    response = 20 * np.log10(mag)
+    response = np.clip(response, -200, 200)
+    return freq1, response
+
+freq1, response = hanning(xam1, 128)
+freq2, response1 = hanning(xam2, 256)
+freq3, response2 = DFT1(xam1, 128)
+freq4, response3 = DFT1(xam1, 256)
+
 
 dft=DFT(xam)
 N = len(dft)
@@ -57,5 +83,12 @@ freq = n/T
 # plt.show()
 plt.plot(freq1, response)
 plt.show()
+# plt.plot(freq2, response1)
+# plt.show()
+plt.plot(freq3, response2)
+plt.show()
+plt.plot(freq4, response3)
+plt.show()
+
 
 
